64a282916ef0de49beb7e02009790bd17f116b50
various.authors <various.authors@gmail.com>
2019-11-22 23:18:46 -0800

    Add: fertile dirt pulls grass to it
    Until forge supports hooks into the spreadable behaviour, fertile dirt
    will have to settle for checking around itself.
    
     - add: blocks added to enderman_holdable.json

--- src/main/java/com/arrogantgamer/fertilesoil/FertileSoil.java
+++ src/main/java/com/arrogantgamer/fertilesoil/FertileSoil.java
@@ -13,8 +13,6 @@ import org.apache.logging.log4j.Logger;
  * v0.2.0
  * [] integrate with all the things (JEI, WAILA, The One Probe, etc...)
  * [?] the farmland should give a bonus?
- * [] can enderman pick it up?
- * [] think about the contrast between the dirt color and the farmland_top color :/
  *  
  * v0.1.0
  * [o] hydration (growth speed should be not as good as farmland)
@@ -48,9 +46,10 @@ import org.apache.logging.log4j.Logger;
  * [o] Farmland's turnToDirt method is static, so I can't override it
  *    so right now, FertileFarmland degenerates into dirt
  *    I need to copy/paste tick/onEntityFallenOn/etc... anything that uses Farmland.turnToDirt
- * [] the dirt color should be richer, because otherwise the blocks 
+ * [o] the dirt color should be richer, because otherwise the blocks 
  *    look identical to dirt in the inventory and that is terrible UX
- * [] let dirt pull grass onto it while we wait for forge
+ * [o] add blocks to enderman_holdable
+ * [o] let dirt pull grass onto it while we wait for forge
  * 
  */
 @Mod(FertileSoil.MODID)
@@ -60,7 +59,5 @@ public final class FertileSoil {
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-    public FertileSoil() {
-        LOGGER.debug("HELLO HELLO");
-    }
+    public FertileSoil() {}
 }
--- src/main/java/com/arrogantgamer/fertilesoil/block/FertileDirtBlock.java
+++ src/main/java/com/arrogantgamer/fertilesoil/block/FertileDirtBlock.java
@@ -1,28 +1,38 @@
 package com.arrogantgamer.fertilesoil.block;
 
+import java.util.Random;
+
 import com.arrogantgamer.fertilesoil.FertileSoil;
+import com.arrogantgamer.fertilesoil.ModBlocks;
 import com.arrogantgamer.fertilesoil.vanilla.behaviours.DirtBlockBehaviours;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SnowBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
+import net.minecraft.world.lighting.LightEngine;
 import net.minecraftforge.common.PlantType;
 import net.minecraftforge.common.ToolType;
 
 /** Fertile Dirt just turns into fertile grass. That's it's whole deal. */
 public class FertileDirtBlock extends Block {
-    public static Block.Properties properties = Block.Properties.create(Material.EARTH, MaterialColor.DIRT).hardnessAndResistance(0.5F).sound(SoundType.GROUND);
+    public static Block.Properties properties = Block.Properties.create(Material.EARTH, MaterialColor.DIRT).hardnessAndResistance(0.5F).sound(SoundType.GROUND).tickRandomly();
 
     public FertileDirtBlock(Properties properties) {
 	super(properties);
@@ -50,4 +60,61 @@ public class FertileDirtBlock extends Block {
     public boolean isToolEffective(BlockState state, ToolType tool) {
 	return tool == ToolType.SHOVEL;
     }
+
+    /*
+     * TODO this is just temporary: the dirt pulls nearby vanilla grass onto it I have a PR
+     * open into forge to add grass growing hooks, but this code will work until
+     * that is merged in
+     * 
+     * @see https://github.com/MinecraftForge/MinecraftForge/pull/6320
+     */
+    @Override
+    public void tick(BlockState state, World worldIn, BlockPos pos, Random random) {
+	// check a random block in the 3x5x3 area above this block
+	// check that this block is OK for grass
+	// set this block to be grass
+
+	if (!worldIn.isRemote) {
+	    for (int i = 0; i < 4; ++i) {
+		// find a grass block: flipped the y so that my dirt is search up for grass
+		// (grass searches down)
+		BlockPos grassPos = pos.add(random.nextInt(3) - 1, random.nextInt(5) - 1, random.nextInt(3) - 1);
+		BlockState grassBlockState = worldIn.getBlockState(grassPos).getBlock().getDefaultState();
+
+		// make sure that this is grass and that it is in bright light
+		if (grassBlockState.getBlock() == Blocks.GRASS_BLOCK && worldIn.getLight(grassPos.up()) >= 9) {
+
+		    // no need to check whether this block is dirt, because it is
+		    // just check whether the conditions are correct for spread
+		    if (func_220256_c(grassBlockState, worldIn, pos)) {
+			// when fertile dirt pulls vanilla grass it becomes fertile grass
+			worldIn.setBlockState(pos, ModBlocks.FERTILE_GRASS_BLOCK.getDefaultState().with(SNOWY, Boolean.valueOf(worldIn.getBlockState(pos.up()).getBlock() == Blocks.SNOW)));
+		    }
+		}
+	    }
+	}
+    }
+
+    /*
+     * TODO technology stolen from SpreadableSnowyDirtBlock to enable the grass spreading
+     * while we wait for forge to implement the hooks
+     */
+    private static final BooleanProperty SNOWY = BlockStateProperties.SNOWY;
+
+    private static boolean func_220257_b(BlockState p_220257_0_, IWorldReader p_220257_1_, BlockPos p_220257_2_) {
+	BlockPos blockpos = p_220257_2_.up();
+	BlockState blockstate = p_220257_1_.getBlockState(blockpos);
+	if (blockstate.getBlock() == Blocks.SNOW && blockstate.get(SnowBlock.LAYERS) == 1) {
+	    return true;
+	} else {
+	    int i = LightEngine.func_215613_a(p_220257_1_, p_220257_0_, p_220257_2_, blockstate, blockpos, Direction.UP, blockstate.getOpacity(p_220257_1_, blockpos));
+	    return i < p_220257_1_.getMaxLightLevel();
+	}
+    }
+
+    private static boolean func_220256_c(BlockState p_220256_0_, IWorldReader p_220256_1_, BlockPos p_220256_2_) {
+	BlockPos blockpos = p_220256_2_.up();
+	return func_220257_b(p_220256_0_, p_220256_1_, p_220256_2_) && !p_220256_1_.getFluidState(blockpos).isTagged(FluidTags.WATER);
+    }
+
 }
